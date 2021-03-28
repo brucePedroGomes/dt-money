1fd4249d2a583945d10e1da82f7203643961c15c
pedro leinar <pedroleinar@hotmail.com>
2021-03-28 19:55:58 -0300

    adjust font size

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,6 +11,17 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box
 }
 
+
+html {
+  @media (max-width: 1080px) {
+    font-size: 93.75%; // 15
+  }
+
+  @media (max-width: 720px) {
+    font-size: 87.5%; // 14
+  }
+}
+
 body {
     background: var(--background);    
 }
