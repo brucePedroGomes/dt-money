179ab63c0765b6d8bbedcdd6a4868d77aa8b5c56
pedro leinar <pedroleinar@hotmail.com>
2021-04-05 05:31:41 -0300

    add mirageJS

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
+import { api } from '../../services/api';
 import { Container } from './style';
 
 export const TransactionsTable = () => {
+  useEffect(() => {
+    api.get('transactions').then((res) => console.log(res.data));
+  }, []);
+
   return (
     <Container>
       <table>
