12302941fab23c7f6545a1cc0204b443162e9293
pedro leinar <pedroleinar@hotmail.com>
2021-04-05 05:56:35 -0300

    feat: add modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
+import modal from 'react-modal';
+
+modal.setAppElement('#root');
 
 export const App = () => {
   return (
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
+import Modal from 'react-modal';
 
 import { Container, Content } from './styles';
 
 export const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModel = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button>Nova transação</button>
+        <button onClick={handleOpenModel}>Nova transação</button>
+        <Modal isOpen={isModalOpen} onRequestClose={handleCloseModel}>
+          <h2>ok</h2>
+        </Modal>
       </Content>
     </Container>
   );
